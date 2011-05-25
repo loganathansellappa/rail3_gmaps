ca108b073e737d1898b2bda1525951a4573f45d9
loganathan <loganathan.ms@gmail.com>
2011-05-25 10:53:32 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Gmaps::Application.config.secret_token = '0c895718193ed1ba2cba25e3fd158b3dde2671bc0b62e059398a49ccdb05ea97b269109125c2f12bfba26f3a5bfd5aa59dfd78650ecba8121e22b901ac0ccf3c'
